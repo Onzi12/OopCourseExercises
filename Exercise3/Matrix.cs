49c77ca9073ae43c79686ee242a9e809fb01dcd6
onzi12 <onzi12@gmail.com>
2016-06-20 04:25:45 +0300

    Excercise 3 finished

--- Exercise3/Matrix.cs
+++ Exercise3/Matrix.cs
@@ -4,25 +4,50 @@ namespace Exercise3
 {
     internal class Matrix<T>
     {
-        T[,] matrix;
-        private int n;
+        private readonly T[,] matrix;
+
+        public int N { get; } // can be assigned only in a constructor
+
+        private readonly Func<object, object, object> addition;
+        private readonly Func<object, object, object> substraction;
+        private readonly Func<object, object, object> multiply;
+
+        // Add two objects of type T 
+        private T TtypeAddition(T x, T y) => (T) addition(x, y);
+
+        // Subtract two objects of type T 
+        private T TtypeSubstraction(T x, T y) => (T) substraction(x, y);
+
+        // Multiply two objects of type T
+        private T TtypeMultiply(T x, T y) => (T) multiply(x, y);
+
 
         public Matrix(
                       int n, 
                       Func<object, object, object> addition, 
-                      Func<object, object, object> substrection, 
-                      Func<object, object, object> doubleMul)
+                      Func<object, object, object> substraction, 
+                      Func<object, object, object> multiply)
         {
             N = n;
             matrix = new T[N,N];
-
+            this.addition = addition;
+            this.substraction = substraction;
+            this.multiply = multiply;
         }
 
-        public int N
+        public void print()
         {
-            get { return n; }
-            private set { n = value; }
+            for ( int i = 0 ; i < N ; i++ )
+            {
+                for ( int j = 0 ; j < N ; j++ )
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
         }
+
+#region operators
 
         public T this[int i, int j]
         {
@@ -30,28 +55,63 @@ namespace Exercise3
             set { matrix[i,j] = value; }
         }
 
-        public void print()
-        {
-            for (int i = 0; i < N; i++)
-            {
-                for (int j = 0; j < N; j++)
-                {
-                    Console.Write(matrix[i,j] + "  ");
-                }
-                Console.WriteLine();
-            }
-        }
-
         public static Matrix<T> operator +(Matrix<T> x, Matrix<T> y)
         {
-            var result = new Matrix<T>(x.N, null, null, null); //TODO: Fix me
+            var result = new Matrix<T>(x.N, x.addition, x.substraction, x.multiply);
+
+            for (int i = 0; i < x.N; i++)
+            {
+                for (int j = 0; j < x.N; j++)
+                {
+                    result[i, j] = result.TtypeAddition(x[i, j], y[i, j]);
+                }
+            }
+            return result;
+        }
+
+        public static Matrix<T> operator -(Matrix<T> x, Matrix<T> y)
+        {
+            var result = new Matrix<T>(x.N, x.addition, x.substraction, x.multiply);
+
+            for ( int i = 0 ; i < x.N ; i++ )
+            {
+                for ( int j = 0 ; j < x.N ; j++ )
+                {
+                    result[i, j] = result.TtypeSubstraction(x[i, j], y[i, j]);
+                }
+            }
             return result;
         }
 
         public static Matrix<T> operator *(Matrix<T> x, Matrix<T> y)
         {
-            var result = new Matrix<T>(x.N, null, null, null); //TODO: Fix me
+            var result = new Matrix<T>(x.N, x.addition, x.substraction, x.multiply);
+
+            for ( int i = 0 ; i < x.N ; i++ )
+            {
+                for ( int j = 0 ; j < x.N ; j++ )
+                {
+                    T res = default(T);
+                    for (int k = 0; k < x.N; k++)
+                    {
+                        res = result.TtypeAddition(result.TtypeMultiply(x[i, k], y[k, j]), res);
+                    }
+                    result[i, j] = res;
+                }
+            }
+
             return result;
         }
+
+        public static explicit operator T(Matrix<T> matrix)
+        {
+            T sum = default(T);
+            foreach (var t in matrix.matrix)
+            {
+                sum = matrix.TtypeAddition(t, sum);
+            }
+            return sum;
+        }
+#endregion operators
     }
 }
